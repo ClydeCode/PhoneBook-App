0daec3f886e2101d926348c913851b67ae9b2c16
Clyde <noneclyde@gmail.com>
2022-11-09 13:52:25 +0200

    Make GetUserInput class static and add GetString function

--- PhoneBook-App/GetUserInput.cs
+++ PhoneBook-App/GetUserInput.cs
@@ -1,21 +1,34 @@
 ﻿
-internal class GetUserInput
+static class GetUserInput
 {
-    internal int GetInt()
+    static internal int GetInt()
     {
         int x = 0;
 
         while(x == 0)
         {
             Console.WriteLine("\nInput a number: ");
-            string? input = Console.ReadLine();
+            string? Input = Console.ReadLine();
 
-            Int32.TryParse(input, out x);
+            Int32.TryParse(Input, out x);
 
             if (x == 0) Console.WriteLine("Wrong format!");
         }
 
         return x;
+    }
+
+    static internal string GetString(string title)
+    {
+        string? Input = "";
+
+        while (Input == "")
+        {
+            Console.WriteLine($"Input a {title}: ");
+            Input = Console.ReadLine();
+        }
+
+        return Input;
     }
 }
 
