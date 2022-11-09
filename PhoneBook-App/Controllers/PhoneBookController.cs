80be47a36fa1c3bacf6dfa40ecefad70b2e81e86
Clyde <noneclyde@gmail.com>
2022-11-09 10:22:18 +0200

    Add PhoneBookController

---
+++ PhoneBook-App/Controllers/PhoneBookController.cs
@@ -0,0 +1,64 @@
+﻿internal class PhoneBookController
+{
+    readonly Database Database = new();
+    readonly TableVisualisationEngine TVEngine = new();
+    internal void ShowMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("1. Show contacts");
+        Console.WriteLine("2. Add contact");
+        Console.WriteLine("3. Delete contact");
+        Console.WriteLine("4. Update contact");
+        Console.WriteLine("5. Exit");
+    }
+
+    internal void Navigate(int Option)
+    {
+        switch (Option)
+        {
+            case 1:
+                ShowContacts();
+                break;
+            case 2:
+                AddContact();
+                break;
+            case 3:
+                DeleteContact();
+                break;
+            case 4:
+                UpdateContact();    
+                break;
+            case 5:
+                Environment.Exit(0);
+                break;
+            default:
+                Console.WriteLine("Wrong Option!");
+                break;
+        }
+    }
+
+    private void ShowContacts()
+    {
+        TVEngine.Add(Database.ReadAll());
+        Console.Clear();
+        TVEngine.Print();
+    }
+
+    private void AddContact()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void DeleteContact()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void UpdateContact()
+    {
+        throw new NotImplementedException();
+    }
+
+
+}
+
