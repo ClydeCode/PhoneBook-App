fffa54cd88707b4e5ff12f09a6894f4144935864
Clyde <noneclyde@gmail.com>
2022-11-09 08:30:59 +0200

    Add Update and Read functions in Database class

--- PhoneBook-App/Database.cs
+++ PhoneBook-App/Database.cs
@@ -29,5 +29,44 @@ internal class Database
             }
         }
     }
+
+    internal void Update(ContactClass Contact)
+    {
+        using (var db = new PhoneBookContext())
+        {
+            var record = db.Contacts.Find(Contact.Id);
+
+            if (record != null)
+            {
+                record.Name = Contact.Name;
+                record.PhoneNumber = Contact.PhoneNumber;
+
+                db.Contacts.Update(record);
+                db.SaveChanges();
+            } 
+            else
+            {
+                Console.WriteLine("This record doesn't exist!");
+            }
+        }
+    }
+
+    internal ContactClass Read(int Id)
+    {
+        ContactClass? Record;
+
+        using (var db = new PhoneBookContext())
+        {
+            
+            Record = db.Contacts
+                .Where(record => record.Id == Id)
+                .Select(record => new ContactClass { Id = record.Id, Name = record.Name, PhoneNumber = record.PhoneNumber })
+                .FirstOrDefault();
+            
+            if (Record == null) Console.WriteLine("This record doesn't exist!");
+        }
+
+        return Record!;
+    }
 }
 
