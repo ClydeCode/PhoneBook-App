d82708ed414eaa113bff92829515f03b6c84a0f4
Clyde <noneclyde@gmail.com>
2022-11-09 10:21:48 +0200

    Add GetUserInput

---
+++ PhoneBook-App/GetUserInput.cs
@@ -0,0 +1,21 @@
+﻿
+internal class GetUserInput
+{
+    internal int GetInt()
+    {
+        int x = 0;
+
+        while(x == 0)
+        {
+            Console.WriteLine("\nInput a number: ");
+            string? input = Console.ReadLine();
+
+            Int32.TryParse(input, out x);
+
+            if (x == 0) Console.WriteLine("Wrong format!");
+        }
+
+        return x;
+    }
+}
+
