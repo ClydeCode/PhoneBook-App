70e55663a247a775edff2af6ff216fe3dbd98288
Clyde <noneclyde@gmail.com>
2022-11-09 14:02:12 +0200

    Add arguments

--- PhoneBook-App/Program.cs
+++ PhoneBook-App/Program.cs
@@ -4,7 +4,7 @@ while (true)
 {
     Program.ShowMenu();
 
-    Program.Navigate(GetUserInput.GetInt());
+    Program.Navigate(GetUserInput.GetInt("Option"));
 
     Console.WriteLine("\nPress ENTER to Continue...");
     Console.ReadLine();
