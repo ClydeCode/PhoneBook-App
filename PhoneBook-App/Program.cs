10132fc6b7c9e58ce9667beb2f5acc26927090f9
Clyde <noneclyde@gmail.com>
2022-11-09 13:53:04 +0200

    Add content in Main class

--- PhoneBook-App/Program.cs
+++ PhoneBook-App/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿PhoneBookController Program = new();
+
+while (true)
+{
+    Program.ShowMenu();
+
+    Program.Navigate(GetUserInput.GetInt());
+
+    Console.WriteLine("\nPress ENTER to Continue...");
+    Console.ReadLine();
+}
